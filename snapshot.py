e6420ff41e5410fddd4d865b01405cba8cfd2d98
Manuga Perera <manuga.20210970@iit.ac.lk>
2026-04-19 11:23:11 +0530

    Remove test snapshot files

--- snapshot.py
+++
@@ -1,9 +0,0 @@
-import cv2
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-if ret:
-    cv2.imwrite('test_snapshot.jpg', frame)
-    print('Saved! Size:', frame.shape)
-else:
-    print('Failed to capture!')
-cap.release()
